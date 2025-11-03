9b0ca5fdb4b87d48f82bb874a0378c75b72d8907
Dalton Fontes <fontes.dalton@hotmail.com>
2025-11-03 19:37:57 -0300

    Add package configuration and documentation settings to mix.exs

--- mix.exs
+++ mix.exs
@@ -6,8 +6,11 @@ defmodule FlowCsv.MixProject do
       app: :flow_csv,
       version: "0.1.0",
       elixir: "~> 1.17",
+      description: "A concurrent and functional CSV parser built with Elixir.",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      docs: [main: FlowCsv]
     ]
   end
 
@@ -22,7 +25,16 @@ defmodule FlowCsv.MixProject do
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Dalton Fontes"],
+      links: %{"GitHub" => "https://github.com/daltonfontes/flow_csv"}
     ]
   end
 end
